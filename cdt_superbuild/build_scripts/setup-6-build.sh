18295dd9ddddaddb6c36900729a3ef8e900d0689
Baskaran Sripathmanathan <baskaran@robots.ox.ac.uk>
2021-03-31 15:02:52 +0100

    adds nodes to exploration graph when you move more than 1m away

--- cdt_superbuild/build_scripts/setup-6-build.sh
+++ cdt_superbuild/build_scripts/setup-6-build.sh
@@ -9,7 +9,7 @@ cd ${WORKING_DIR}/catkin_ws
 source /opt/ros/melodic/setup.bash
 
 echo "Building catkin workspace"
-catkin build --continue-on-failure
+catkin build --continue-on-failure --cmake-args -DCMAKE_CXX_COMPILER="/usr/bin/g++" -DCMAKE_C_COMPILER="/usr/bin/gcc"
 if [ $? -ne 0 ]; then
     echo "Build of catkin workspace failed"
     exit 7
